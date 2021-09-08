f86c9ac318bf8d3cbf298fe0fccc37c2da12c2c8
Moritz Wolter <moritz@wolter.tech>
2021-09-08 17:59:17 +0200

    convolution matrix 2d.

--- tests/test_boundary_filters.py
+++ tests/test_boundary_filters.py
@@ -1,7 +1,9 @@
+# Created by moritz ( wolter@cs.uni-bonn.de ), 08.09.21
 import pywt
 import torch
 import pytest
 import numpy as np
+from scipy import misc
 import matplotlib.pyplot as plt
 
 from src.ptwt.matmul_transform import (
@@ -32,7 +34,97 @@ def test_boundary_filter_analysis_and_synthethis_matrices():
 
 
 
+def construct_conv2d_matrix(filter: torch.tensor, input_rows: int,
+        input_columns: int, dtype=torch.float64):
+    """ Create a two dimensional convolution matrix. 
+        Convolving with this matrix should be equivalent to
+        a call to scipy.signal.convolve2d and a reshape.
+
+    Args:
+        filter (torch.tensor): The filter to convolve with.
+        input_rows (int): The number of rows in the input matrix.
+        input_columns (int): The number of columns in the input matrix.
+        dtype (optional): Input data type. Defaults to torch.float64.
+
+    Returns:
+        [torch.sparse.FloatTensor]: A sparse convolution matrix.
+    """    
+    filter_rows, filter_columns = filter.shape
+    
+    block_height = input_rows + filter_rows - 1
+    block_width = input_columns
+    block_entries = input_rows*filter_rows
+
+    all_entries = filter_columns*input_columns*block_entries
+
+    sparse_columns = np.zeros([all_entries])
+    sparse_rows = np.zeros([all_entries])
+    sparse_entries = torch.zeros([all_entries])
+
+    matrix_height = (input_columns + filter_columns - 1)*block_height 
+    matrix_width = input_columns*block_width
+
+    col = np.stack([np.arange(0, input_rows)]*filter_rows) # + 1
+    row = col + np.arange(0, filter_rows)[:, np.newaxis]
+    col = col.flatten()
+    row = row.flatten()
+    row = np.stack([row]*input_columns, -1)
+    col = np.stack([col]*input_columns, -1)
+
+    column_offset = np.arange(0, input_columns)*input_rows
+    column_offset = np.stack([column_offset]*(input_rows*filter_rows))
+    column_offset = column_offset + col
+    column_offset = column_offset.T.flatten()
+    row_offset = np.arange(0, input_columns)*block_height
+    row_offset = np.stack([row_offset]*(input_rows*filter_rows))
+    row_offset = row_offset + row
+    row_offset = row_offset.T.flatten()
+
+    for col in range(0, filter_columns):
+        entries = filter[:, col]
+        entries = torch.stack([entries]*input_rows).flatten() # double check T?
+        entries = torch.stack([entries]*input_columns).flatten()
+        start = col*input_columns*block_entries
+        stop = start + input_columns*block_entries
+
+        sparse_rows[start:stop] = row_offset 
+        sparse_columns[start:stop] = column_offset
+        sparse_entries[start:stop] = entries
+        row_offset += block_height
+
+    sparse_indices = np.stack([sparse_rows, sparse_columns])
+    matrix = torch.sparse_coo_tensor(sparse_indices, sparse_entries, dtype=dtype)
+    assert (matrix_height, matrix_width) == matrix.shape
+    # plt.imshow(matrix.to_dense()); plt.show()
+    return matrix
+
+
 if __name__ == '__main__':
-    # test_cyclic_analysis_and_synthethis_matrices_db8()
-    test_boundary_filter_analysis_and_synthethis_matrices()
+    import torch
+    import scipy.signal
+
+    filter_shape = [3,3]
+    filter = torch.ones(filter_shape)/9.
+    filter = filter.unsqueeze(0).unsqueeze(0)
+    face = misc.face()[256:(256+5), 256:(256+5)]
+    face = np.mean(face, -1)
+
+    # res = torch.nn.functional.conv2d(input=face, weight=filter)
+    res = scipy.signal.convolve2d(face, filter.squeeze().numpy())
+
+    conv_matrix2d = construct_conv2d_matrix(filter.squeeze(), 5, 5, torch.float32)
+
+    plt.imshow(conv_matrix2d.to_dense().numpy())
+    plt.show()
+    face = torch.from_numpy(face.astype(np.float32))
+    face = face.unsqueeze(0).unsqueeze(0)
+    res_flat = torch.sparse.mm(conv_matrix2d, face.flatten().unsqueeze(-1))
+    res_mm = torch.reshape(res_flat, [filter_shape[0] + 5 - 1, filter_shape[1] + 5 - 1])
+
+    diff = np.abs(res - res_mm.numpy())
+    plot = np.concatenate([res, res_mm.numpy(), diff], -1)
+    plt.imshow(plot)
+    plt.show()
+
     print('stop')
+
