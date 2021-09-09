3278fc001340eb10b22d40b78f447ed81fb9659b
Moritz Wolter <moritz@wolter.tech>
2021-09-09 16:08:02 +0200

    flake8

--- tests/test_boundary_filters.py
+++ tests/test_boundary_filters.py
@@ -41,9 +41,10 @@ def test_boundary_filter_analysis_and_synthethis_matrices():
 
 
 def test_mean_conv_matrix_2d():
-
-    for filter_shape in [(3,3), (3,2), (2,3), (5,3), (3,5), (2,5), (5,2)]:
-        for size in [(64, 64), (32, 64), (64, 32), (64, 31), (31, 64), (65, 65)]:
+    for filter_shape in [(3, 3), (3, 2), (2, 3), (5, 3), (3, 5),
+                         (2, 5), (5, 2)]:
+        for size in [(64, 64), (32, 64), (64, 32), (64, 31), (31, 64),
+                     (65, 65)]:
             # size = (256, 256)
             # filter = torch.ones(filter_shape)/9.
             filter = torch.rand(filter_shape)
@@ -54,33 +55,30 @@ def test_mean_conv_matrix_2d():
             # res = torch.nn.functional.conv2d(input=face, weight=filter)
             res = scipy.signal.convolve2d(face, filter.squeeze().numpy())
 
-            conv_matrix2d = construct_conv2d_matrix(filter.squeeze(), size[0], size[1], torch.float32)
+            conv_matrix2d = construct_conv2d_matrix(
+                filter.squeeze(), size[0], size[1], torch.float32)
 
             # plt.imshow(conv_matrix2d.to_dense().numpy())
             # plt.show()
             face = torch.from_numpy(face.astype(np.float32))
             face = face.unsqueeze(0).unsqueeze(0)
-            res_flat = torch.sparse.mm(conv_matrix2d, face.T.flatten().unsqueeze(-1))
-            # res_mm = np.reshape(res_flat.numpy(),
-            #     [filter_shape[0] + size[0] - 1, filter_shape[1] + size[1] - 1], order='F')
+            res_flat = torch.sparse.mm(
+                conv_matrix2d, face.T.flatten().unsqueeze(-1))
             res_mm = torch.reshape(res_flat,
-                  [filter_shape[1] + size[1] - 1, filter_shape[0] + size[0] - 1]).T
-
+                                   [filter_shape[1] + size[1] - 1,
+                                    filter_shape[0] + size[0] - 1]).T
 
             diff = np.mean(np.abs(res - res_mm.numpy()))
-            print(size, filter_shape, '%2.2e.'%diff, np.allclose(res, res_mm.numpy()))
+            print(size, filter_shape, '%2.2e.' % diff,
+                  np.allclose(res, res_mm.numpy()))
             assert np.allclose(res, res_mm)
 
 
-
 if __name__ == '__main__':
-    import torch
-    import scipy.signal
-
-    filter_shape = [3,3]
+    filter_shape = [3, 3]
     size = (5, 5)
     # filter = torch.ones(filter_shape)/9.
-    filter = torch.tensor([[1,2,3], [4,5,6], [7,8,9]])
+    filter = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     # filter = torch.rand(filter_shape)
     filter = filter.unsqueeze(0).unsqueeze(0)
     face = misc.face()[256:(256+size[0]), 256:(256+size[1])]
@@ -89,7 +87,8 @@ if __name__ == '__main__':
     # res = torch.nn.functional.conv2d(input=face, weight=filter)
     res = scipy.signal.convolve2d(face, filter.squeeze().numpy())
 
-    conv_matrix2d = construct_conv2d_matrix(filter.squeeze(), size[0], size[1], torch.float32)
+    conv_matrix2d = construct_conv2d_matrix(
+        filter.squeeze(), size[0], size[1], torch.float32)
 
     # plt.spy(conv_matrix2d.to_dense().numpy(), marker='.')
     # plt.imshow(conv_matrix2d.to_dense().numpy())
@@ -98,7 +97,8 @@ if __name__ == '__main__':
     face = face.unsqueeze(0).unsqueeze(0)
     res_flat = torch.sparse.mm(conv_matrix2d, face.T.flatten().unsqueeze(-1))
     res_mm = np.reshape(res_flat,
-        [filter_shape[0] + size[0] - 1, filter_shape[1] + size[1] - 1]).T
+        [filter_shape[0] + size[0] - 1,
+         filter_shape[1] + size[1] - 1]).T
 
     diff = np.abs(res - res_mm.numpy())
     print(np.mean(diff), np.allclose(res, res_mm.numpy()))
