2df8f4ebe5b6eef8f3fcd28be58469a95bb5b7e0
Moritz Wolter <moritz@wolter.tech>
2021-09-28 16:54:53 +0200

    update init.py

--- src/ptwt/__init__.py
+++ src/ptwt/__init__.py
@@ -1,3 +1,4 @@
 from .conv_transform import wavedec, wavedec2, waverec, waverec2
 from .matmul_transform import matrix_wavedec, matrix_waverec
 from .packets import WaveletPacket, WaveletPacket2D
+from .matmul_transform_2d import MatrixWavedec2d, MatrixWaverec2d
