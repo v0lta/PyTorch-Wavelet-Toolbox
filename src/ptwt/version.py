41a474c62cdee70b09243e73250a13dc1b841652
Moritz Wolter <moritz@wolter.tech>
2021-10-14 19:56:48 +0200

    lint more of the docs.

--- src/ptwt/version.py
+++ src/ptwt/version.py
@@ -33,7 +33,7 @@ def get_git_hash() -> str:
 
 
 def get_version(with_git_hash: bool = False):
-    """Get the :mod:`freqdect` version string, including a git hash."""
+    """Get the :mod:`ptwt` version string, including a git hash."""
     return f"{VERSION}-{get_git_hash()}" if with_git_hash else VERSION
 
 
