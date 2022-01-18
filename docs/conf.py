a4b8b2d725bf457d8eecd9081ae04427806d9b5e
Moritz Wolter <moritz@wolter.tech>
2022-01-18 12:38:28 +0100

    add __init__ to documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# document __init__ in the docpages
+autoclass_content = 'both'
+
 
 # -- Options for HTML output -------------------------------------------------
 
