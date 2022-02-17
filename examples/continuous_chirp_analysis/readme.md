80618b64c697693c0ee2c1aff49fc84b37a6e3fd
Moritz Wolter <moritz@wolter.tech>
2022-02-17 14:26:56 +0100

    cwt test and example.

---
+++ examples/continuous_chirp_analysis/readme.md
@@ -0,0 +1,7 @@
+#### Cwt chirp analysis
+To run this example, clone the repository and type
+```ipython examples/wavelet_packet_chirp_analysis/cwt_chirp_analysis.py```
+
+The result should look like this:
+
+![alt text](chirp_cwt.png)
--- tests/test_cwt.py
+++ tests/test_cwt.py
@@ -1,16 +1,60 @@
+"""Test the continuous transformation code."""
+
 import numpy as np
+import pytest
+import pywt
+import torch
 from scipy import signal
+from src.ptwt.continuous_transform import cwt
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+continuous_wavelets = [
+    "cgau1",
+    "cgau2",
+    "cgau3",
+    "cgau4",
+    "cgau5",
+    "cgau6",
+    "cgau7",
+    "cgau8",
+    "gaus1",
+    "gaus2",
+    "gaus3",
+    "gaus4",
+    "gaus5",
+    "gaus6",
+    "gaus7",
+    "gaus8",
+    "mexh",
+    "morl",
+]
 
+
+@pytest.mark.parametrize("cuda", [False, True])
+@pytest.mark.parametrize("wavelet", continuous_wavelets)
+def test_cwt(wavelet, cuda):
+    """Test the cwt implementation for various wavelets and with a GPU."""
     t = np.linspace(-2, 2, 800, endpoint=False)
-    sig  = np.cos(2 * np.pi * 7 * t) + signal.gausspulse(t - 0.4, fc=2)
     sig = signal.chirp(t, f0=1, f1=50, t1=10, method="linear")
     widths = np.arange(1, 31)
-    cwtmatr = signal.cwt(sig, signal.ricker, widths)
-    fig, axs = plt.subplots(2)
-    axs[0].plot(t, sig)
-    axs[1].imshow(cwtmatr, cmap='PRGn', aspect='auto',
-               vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
-    plt.show()
+    cwtmatr, freqs = pywt.cwt(sig, widths, wavelet)
+    sig = torch.from_numpy(sig)
+    if cuda:
+        if torch.cuda.is_available():
+            sig = sig.cuda()
+    cwtmatr_pt, freqs_pt = cwt(sig, widths, wavelet)
+    if cuda:
+        cwtmatr_pt = cwtmatr_pt.cpu()
+    assert np.allclose(cwtmatr_pt.numpy(), cwtmatr)
+    assert np.allclose(freqs, freqs_pt)
+
+
+@pytest.mark.parametrize("wavelet", continuous_wavelets)
+def test_cwt_batched(wavelet):
+    """Test batched transforms."""
+    sig = np.random.randn(10, 200)
+    widths = np.arange(1, 30)
+    cwtmatr, freqs = pywt.cwt(sig, widths, wavelet)
+    sig = torch.from_numpy(sig)
+    cwtmatr_pt, freqs_pt = cwt(sig, widths, wavelet)
+    assert np.allclose(cwtmatr_pt.numpy(), cwtmatr)
+    assert np.allclose(freqs, freqs_pt)
