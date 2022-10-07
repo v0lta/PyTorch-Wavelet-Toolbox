precision fix.

src/ptwt/continuous_transform.py
@@ -66,7 +66,7 @@ def cwt(
         >>> )
     """
     # accept array_like input; make a copy to ensure a contiguous array
-    if not isinstance(wavelet, (ContinuousWavelet, Wavelet, DifferentiableWavelet)):
+    if not isinstance(wavelet, (ContinuousWavelet, Wavelet, DifferentiableContinuousWavelet)):
         wavelet = DiscreteContinuousWavelet(wavelet)
     if type(scales) is torch.Tensor:
         scales = scales.numpy()
@@ -122,7 +122,7 @@ def cwt(
     out_tensor = torch.stack(out)
     if type(wavelet) is Wavelet:
         out_tensor = out_tensor.real
-    elif isinstance(wavelet, DifferentiableWavelet):
+    elif isinstance(wavelet, DifferentiableContinuousWavelet):
         out_tensor = out_tensor # TODO: fixme
     else:
         out_tensor = out_tensor if wavelet.complex_cwt else out_tensor.real
@@ -178,7 +178,7 @@ def _integrate_wavelet(wavelet, precision=8):
         msg = ("Integration of a general signal is deprecated "
                "and will be removed in a future version of pywt.")
         warnings.warn(msg, DeprecationWarning)
-    elif not isinstance(wavelet, (Wavelet, ContinuousWavelet, DifferentiableWavelet)):
+    elif not isinstance(wavelet, (Wavelet, ContinuousWavelet, DifferentiableContinuousWavelet)):
         wavelet = DiscreteContinuousWavelet(wavelet)
 
     if type(wavelet) in (tuple, list):
@@ -204,10 +204,12 @@ def _integrate_wavelet(wavelet, precision=8):
         return _integrate(psi_d, step), _integrate(psi_r, step), x
 
 
-class DifferentiableWavelet(torch.nn.Module, ContinuousWavelet):
+class DifferentiableContinuousWavelet(torch.nn.Module, ContinuousWavelet):
+    """A base class for learnable Continuous Wavelets."""    
     pass
 
-class Shannon_Wavelet(DifferentiableWavelet):
+
+class Shannon_Wavelet(DifferentiableContinuousWavelet):
     """A differentiable Shannon wavelet."""
 
     def __init__(self, name='shan1-1'):
@@ -218,8 +220,11 @@ class Shannon_Wavelet(DifferentiableWavelet):
             center (int): _description_
         """
         super().__init__()
-        self.bandwidth = torch.tensor(self.bandwidth_frequency)
-        self.center = torch.tensor(self.center_frequency)
+        self.dtype = torch.float64
+        # Use torch.nn.Parameter
+        self.bandwidth = torch.tensor(self.bandwidth_frequency,
+            dtype=self.dtype)
+        self.center = torch.tensor(self.center_frequency, dtype=self.dtype)
 
     def __call__(self, grid_values: torch.Tensor) -> torch.Tensor:
         shannon = \

tests/test_cwt.py
@@ -79,16 +79,19 @@ def test_cwt_batched(wavelet):
     assert np.allclose(freqs, freqs_pt)
 
 
-def test_nn_schannon_wavefun(grid_size: int = 10):
+@pytest.mark.parametrize("type", ['shan1-1'])
+@pytest.mark.parametrize("grid_size", [8, 9, 10])
+def test_nn_schannon_wavefun(type: str, grid_size: int):
     
-    pywt_shannon = pywt.ContinuousWavelet('shan1-1')
-    ptwt_shannon = Shannon_Wavelet('shan1-1')
+    pywt_shannon = pywt.ContinuousWavelet(type)
+    ptwt_shannon = Shannon_Wavelet(type)
 
     pywt_sample = pywt_shannon.wavefun(grid_size)
     ptwt_sample = ptwt_shannon.wavefun(grid_size)
 
+    assert np.allclose(pywt_sample[0], ptwt_sample[0].numpy())
     assert np.allclose(pywt_sample[1], ptwt_sample[1].numpy())
-    assert np.allclose(pywt_sample[0], ptwt_sample[0].numpy(), atol=1e-5) # TODO: Fixme!
+
 
 
 
@@ -104,6 +107,6 @@ def test_nn_cwt(
     scales_np = scales.numpy() if type(scales) is torch.Tensor else scales
     cwtmatr, freqs = pywt.cwt(data=sig, scales=scales_np, wavelet=pywt_shannon)
     sig = torch.from_numpy(sig)
-    cwtmatr_pt, freqs_pt = cwt(sig, scales, ptwt_shannon)
+    cwtmatr_pt, freqs_pt = cwt(data=sig, scales=scales, wavelet=ptwt_shannon)
     assert np.allclose(np.abs(cwtmatr_pt.numpy()), np.abs(cwtmatr)) # TODO: Fixme!!
     assert np.allclose(freqs, freqs_pt)