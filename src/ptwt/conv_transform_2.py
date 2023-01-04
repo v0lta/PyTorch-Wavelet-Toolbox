519ed8dd3fc7985a0566b5b6ecdd2c8fb35b0d71
moritz@wolter.tech <moritz@wolter.tech>
2023-01-04 18:28:23 +0100

    remove unnecessary generic type.

--- src/ptwt/conv_transform_2.py
+++ src/ptwt/conv_transform_2.py
@@ -5,10 +5,7 @@ torch.nn.functional.conv_transpose2d under the hood.
 """
 
 
-from typing import List, Optional, Tuple, Union, TypeVar
-
-TensorOrTensorList = TypeVar("TensorOrTensorList", torch.Tensor, List[torch.Tensor])
-
+from typing import List, Optional, Tuple, Union
 import pywt
 import torch
 
