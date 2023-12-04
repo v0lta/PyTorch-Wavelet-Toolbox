fbce176b49898dec88a0f171c2d14282644d1efd
Moritz Wolter <moritz@wolter.tech>
2023-12-04 11:25:29 +0100

    test update.

--- examples/speed_tests/run_all.sh
+++ examples/speed_tests/run_all.sh
@@ -1,6 +1,8 @@
 python timeitconv_1d.py
+python timeitconv_1dmat.py
 python timeitconv_2d.py
 python timeitconv_2d_separable.py
+python timeitconv_2dmat.py
 python timeitconv_3d.py
 python timeitcwt_1d.py
 
