0a2b467d039efb5d2e87d29a0c27e7f546b82593
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-01-25 16:39:08 +0100

    Update test_convolution_fwt_3.py

--- tests/test_convolution_fwt_3.py
+++ tests/test_convolution_fwt_3.py
@@ -1,5 +1,5 @@
 """Test our 3d for loop-convolution based fwt code."""
-import typing
+
 from typing import List
 
 import numpy as np
@@ -8,7 +8,6 @@ import pywt
 import torch
 
 import ptwt
-from ptwt.constants import BoundaryMode
 
 
 def _expand_dims(batch_list: List) -> List:
