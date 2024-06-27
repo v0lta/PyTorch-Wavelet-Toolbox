41aeaa218b1ead966ed28907f87e394aeedf1078
Moritz Wolter <moritz@wolter.tech>
2024-06-27 15:51:40 +0200

    add test.

--- tests/test_packets.py
+++ tests/test_packets.py
@@ -588,3 +588,24 @@ def test_separable_conv_packets_2d(axes: tuple[int, int]) -> None:
     ptwp.initialize(ptwp.get_natural_order(2))
     ptwp.reconstruct()
     assert np.allclose(signal, ptwp[""].data[:, :32, :32, :32])
+
+
+
+def test_partial_reconstruction() -> None:
+
+    signal = np.random.randn(1, 16)
+    signal2 = np.cos(np.linspace(0, 2 * np.pi, 16))
+    ptwp = WaveletPacket(torch.from_numpy(signal), "haar",
+        mode="reflect", maxlevel=2)
+    ptwp.initialize(["aa", "ad", "da", "dd"])
+    
+    ptwp2 = WaveletPacket(torch.from_numpy(signal2), "haar", mode="reflect", maxlevel=2)
+    
+    # overwrite the first packet set.
+    ptwp["aa"] = ptwp2["aa"]
+    ptwp["ad"] = ptwp2["ad"]
+    ptwp["da"] = ptwp2["da"]
+    ptwp["dd"] = ptwp2["dd"]
+    ptwp.reconstruct()
+
+    assert np.allclose(signal2, ptwp[""].numpy()[:16])
