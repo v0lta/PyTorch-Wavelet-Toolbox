f15c6e60be76cea970a8ef4af9e0508b20a17778
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-12-16 15:35:32 +0100

    Update _util.py

--- src/ptwt/_util.py
+++ src/ptwt/_util.py
@@ -794,4 +794,4 @@ def _deprecated_alias(
 
 def _group_for_symmetric(padding: tuple[int, ...]) -> list[tuple[int, int]]:
     """Repack the padding tuple for symmetric padding."""
-    return list(reversed(list(grouper(padding, 2))))
+    return list(reversed(list(grouper(padding, 2))))  # type:ignore[arg-type]
